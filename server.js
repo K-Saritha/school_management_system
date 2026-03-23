0623b9bc4632614290f027f2931cd0e72d7c5e42
saritha <saritha_kapalavaye@srmap.edu.in>
2026-03-23 18:38:26 +0530

    server updated

--- server.js
+++ server.js
@@ -298,8 +298,8 @@ app.get("/listSchools", (req, res) => {
 
 
 
+const PORT = process.env.PORT || 3000;
 
-
-    app.listen(process.env.PORT ||3000, () => {
-        console.log(`Server running on port ${process.env.PORT}`);
-    });
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
