f82df7459634b086c70d83d611b0d4029298d1d1
saritha <saritha_kapalavaye@srmap.edu.in>
2026-03-23 19:04:45 +0530

    port updated

--- server.js
+++ server.js
@@ -296,11 +296,8 @@ app.get("/listSchools", (req, res) => {
 
 
 
-
-
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
-
