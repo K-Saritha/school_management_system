1841e31ad11b54be6ba333698fb86ff6c097c955
saritha <saritha_kapalavaye@srmap.edu.in>
2026-03-23 22:13:17 +0530

    db added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mysql = require("mysql2");
 const multer = require("multer");
-
+require("./db");
 const app = express();
 
 app.use(express.json());
