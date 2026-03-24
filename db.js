1e06cae90d2dda0a2be6137e88a36101cdccbde4
saritha <saritha_kapalavaye@srmap.edu.in>
2026-03-24 10:50:37 +0530

    added constraint

--- db.js
+++ db.js
@@ -1,71 +1,4 @@
-// require("dotenv").config();
-// const mysql = require("mysql2");
 
-// const connection = mysql.createConnection({
-//     host: process.env.MYSQLHOST,
-//     user: process.env.MYSQLUSER,
-//     password: process.env.MYSQLPASSWORD,
-//     database: process.env.MYSQLDATABASE,
-//     port: process.env.MYSQLPORT
-// });
-
-// connection.connect((err) => {
-//     if (err) {
-//         console.log("Connection failed:", err);
-//         return;
-//     }
-
-//     console.log("Connected to MySQL");
-
-//     connection.query(
-//         "CREATE DATABASE IF NOT EXISTS school_management_system",
-//         (err, result) => {
-//             if (err) {
-//                 console.log(err);
-//                 return;
-//             }
-
-//             console.log(result);
-//             console.log("Database ready");
-
-           
-//             connection.query(
-//                 "USE school_management_system",
-//                 (err) => {
-//                     if (err) {
-//                         console.log(err);
-//                         return;
-//                     }
-
-//                     console.log("Using database");
-
-//                     const createTableQuery = `
-//                         CREATE TABLE IF NOT EXISTS schools (
-//                             id INT AUTO_INCREMENT PRIMARY KEY,
-//                             name VARCHAR(100) NOT NULL,
-//                             address VARCHAR(255) NOT NULL,
-//                             latitude FLOAT NOT NULL,
-//                             longitude FLOAT NOT NULL
-//                         )
-//                     `;
-
-//                     connection.query(createTableQuery, (err, result) => {
-//                         if (err) {
-//                             console.log(err);
-//                             return;
-//                         }
-
-//                         console.log(result);
-//                         console.log("Schools table created");
-                        
-                       
-//                     });
-//                 }
-//             );
-
-//         }
-//     );
-// });
 
 require("dotenv").config();
 const mysql = require("mysql2");
@@ -92,7 +25,9 @@ connection.connect((err) => {
             name VARCHAR(100) NOT NULL,
             address VARCHAR(255) NOT NULL,
             latitude FLOAT NOT NULL,
-            longitude FLOAT NOT NULL
+            longitude FLOAT NOT NULL,
+            
+            UNIQUE (latitude, longitude)
         )
     `;
 
